dfad1bf93f38cd83dcd90e219b90088003770af4
kwambokaB <paula.paulatek@gmail.com>
2020-11-24 14:07:37 +0300

    setting up error handling

--- graphql-ts-server-boilerplate/src/modules/register/resolvers.ts
+++ graphql-ts-server-boilerplate/src/modules/register/resolvers.ts
@@ -7,12 +7,21 @@ export const resolvers : ResolverMap = {
     
     Mutation :{
         register: async (_, {email, password} : GQL.IRegisterOnMutationArguments) => {
+            const userAlreadyExists = await User.findOne({where: {email}, select: ["id"]});
+            if (userAlreadyExists) {
+                return [
+                 {
+                     path: "email",
+                     message: "user with this email already exists"
+                 }
+                ]
+            }
            const hashpassword = await bcrypt.hash(password, 10);
            const user = User.create({
                email, password: hashpassword
            });
            user.save();
-           return true;
+           return null;
         }
     }
   }
