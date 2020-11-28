b5cdba7d2289ad15ef99ca712d76466d3c58478c
kwambokaB <paula.paulatek@gmail.com>
2020-11-28 10:57:10 +0300

    refactoring createConfirmEmailLink test

--- graphql-ts-server-boilerplate/src/utils/createConfirmEmailLink.test.ts
+++ graphql-ts-server-boilerplate/src/utils/createConfirmEmailLink.test.ts
@@ -22,19 +22,26 @@ beforeAll(async () => {
    afterAll(async () => {
        connection.close();
    });
-
+   
+describe("Testing confirm email link", () => {
+// confirm that link works
 test("Make sure createConfirmLink works", async() => {
-    // confirm that link works
     const url = await createConfirmEmailLink(process.env.TEST_HOST as string, userId, redis);
     const response  = await fetch(url);
     const text = await response.text();
     expect(text).toEqual('ok');
-    //confirm equal to true
-    const user = await User.findOne({where: {id: userId}});
+});
+//confirm equal to true
+test("Make sure confirm equal to true", async() => {
+   const user = await User.findOne({where: {id: userId}});
     expect((user as User).confirmed).toBeTruthy();
-    // confirm id is removed from redis
+});
+// confirm id is removed from redis
+test("Make sure confirm equal to true", async() => {
+    const url = await createConfirmEmailLink(process.env.TEST_HOST as string, userId, redis);
     const chunks = url.split("/");
     const key = chunks[chunks.length -1];
     const value = await redis.get(key);
     expect(value).toBeNull
-})
+});
+});
